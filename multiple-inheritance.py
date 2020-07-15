31243e4c3c0e739987aab01036d9297bf907b70e
Aman.Kesarwani <amank2410@gmail.com>
2020-07-15 11:58:42 +0530

    multiple inheritance - good vs bad

---
+++ multiple-inheritance.py
@@ -0,0 +1,39 @@
+class Employee:
+    def greet(self):
+        print("Employee Greet")
+
+
+class Person:
+    def greet(self):
+        print("Person Greet")
+
+
+class Manager(Person, Employee):
+    pass
+
+
+manager = Manager()
+manager.greet()  # Person greet will be called as per order of inheritance
+
+# Below muliple inheritance usage is logically correct
+
+
+class FlyingCreature:
+    def fly(self):
+        print("Fly")
+
+
+class SwimmingCreature:
+    def swim(self):
+        print("swim")
+
+
+class FlyingFish(FlyingCreature, SwimmingCreature):
+    def eat(self):
+        print("Eat")
+
+
+fish = FlyingFish()
+fish.fly()
+fish.swim()
+fish.eat()
