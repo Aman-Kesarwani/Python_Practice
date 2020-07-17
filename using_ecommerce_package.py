47749004acc0587273e0412ec3fd0f7b1ff115e7
Aman.Kesarwani <amank2410@gmail.com>
2020-07-17 12:30:04 +0530

    create package and use module from it

---
+++ using_ecommerce_package.py
@@ -0,0 +1,11 @@
+from ecommerce.sales_1 import calculate_shipping, calulate_tax
+from ecommerce import sales_1
+
+#import ecommerce.sales_1
+
+# ecommerce.sales_1.calculate_shipping()
+
+calulate_tax()
+calculate_shipping()
+sales_1.calculate_shipping()
+sales_1.calulate_tax()
