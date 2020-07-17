27b1ce7c9ac29a71bb0d87f51d877a81ee4cdd54
Aman.Kesarwani <amank2410@gmail.com>
2020-07-17 12:52:22 +0530

    using sub package

---
+++ ecommerce_1/shopping/the_sales.py
@@ -0,0 +1,6 @@
+def calulate_tax():
+    print("Calculate Tax")
+
+
+def calculate_shipping():
+    print("Calculate Shipping")
---
+++ using_subpackage.py
@@ -0,0 +1,7 @@
+from ecommerce_1.shopping import the_sales
+from ecommerce_1.shopping.the_sales import calulate_tax
+
+calulate_tax()
+
+
+the_sales.calulate_tax()
