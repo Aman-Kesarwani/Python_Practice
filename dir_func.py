81a4d9a3b5a81abceacb0bb83ba62b3781a4d69f
Aman.Kesarwani <amank2410@gmail.com>
2020-07-19 14:49:20 +0530

    dir function

---
+++ dir_func.py
@@ -0,0 +1,7 @@
+from ecommerce_1.shopping import the_sales
+
+print(dir(the_sales))
+print("name", the_sales.__name__)
+print("file", the_sales.__file__)
+print("package", the_sales.__package__)
+print("doc", the_sales.__doc__)
